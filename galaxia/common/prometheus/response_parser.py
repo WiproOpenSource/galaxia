b25c42277fec5b6dbf7a6df65b4e067ff402383e
Ashish Jain <ashish.jain14@wipro.com>
2016-04-27 13:10:08 +0000

    Added prometheus response parser

---
+++ galaxia/common/prometheus/response_parser.py
@@ -0,0 +1,43 @@
+# Copyright 2016 - Wipro Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Prometheus response parser
+"""
+import json
+
+
+def get_names_list(resp):
+    names_list = []
+    metrics_list = []
+    hosts_list = []
+    result_list = json.loads(resp)['data']['result']
+
+    for i in result_list:
+        if i['metric'].get('image'):
+            names_list.append(i['metric'].get('name'))
+            metrics_list.append(i['value'][1])
+            hosts_list.append(i['metric'].get('instance').split(':')[0])
+
+    return names_list, metrics_list, hosts_list
+
+
+def get_node_name_list(resp):
+    names_list = []
+    result_list = json.loads(resp)['data']['result']
+
+    for i in result_list:
+        names_list.append(i['metric'].get('instance').split(':')[0])
+
+    return names_list
