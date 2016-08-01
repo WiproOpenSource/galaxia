a492fc0144a71bed2998e078af6e355b1828caf8
Ashish Jain <ashish.jain14@wipro.com>
2016-08-01 19:29:53 +0000

    Changes to label api to segregate label retrieval for node, container and app

--- galaxia/gapi/controller/v1/label.py
+++ galaxia/gapi/controller/v1/label.py
@@ -11,6 +11,9 @@ class LabelController(object):
     @expose(generic=True)
     def index(self):
         meter_name = request.GET.get('meter_name')
+        unit_type = request.GET.get('unit_type')
+        search_type = request.GET.get('search_type')
+        search_string = request.GET.get('search_string')
         handler = label_handler.LabelHandler()
-        labels_list = handler.get_labels(meter_name)
+        labels_list = handler.get_labels(meter_name, unit_type, search_type, search_string)
         return labels_list
--- galaxia/gapi/handler/v1/label_handler.py
+++ galaxia/gapi/handler/v1/label_handler.py
@@ -8,7 +8,40 @@ log = logging.getLogger(__name__)
 
 class LabelHandler(object):
 
-    def get_labels(self, meter_name):
+    def retrieve_labels(self, meter_name):
         labels_list = prometheus_helper.get_labels(meter_name)
         return json.dumps(labels_list)
+
+    def get_labels(self, meter_name, unit_type, search_type, search_string):
+        #unit_type = kwargs['unit_type']
+        if unit_type in self._function:
+            return self._function[unit_type](meter_name, search_type, search_string)
+
+    @property
+    def _function(self):
+        return dict((attr, getattr(self, attr))
+                    for attr in dir(self)
+                    if not attr.startswith('_') and callable(getattr(self, attr
+                                                                     )))
+
+    def app(self, meter_name, search_type, search_string):
+        if search_type is not None and search_string is not None:
+            meter = meter_name+"{"+ search_type +"="+'"'+search_string+'"'+"}"
+        else:
+            meter = meter_name
+        return self.retrieve_labels(meter)
+
+    def node(self, meter_name, search_type, search_string):
+        if search_type is not None and search_string is not None:
+            meter = meter_name+"{"+ search_type +"="+'"'+search_string+'"'+"}"
+        else:
+            meter = meter_name
+        return self.retrieve_labels(meter)
+
+    def container(self, meter_name, search_type, search_string):
+        if search_type is not None and search_string is not None:
+            meter = meter_name+"{"+ search_type +"="+'"'+search_string+'"'+"}"
+        else:
+            meter = meter_name
+        return self.retrieve_labels(meter)
 
